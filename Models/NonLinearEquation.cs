004c758d7490c44acb56d0e4a6e2eba8e857b59b
IrinaX6m <tgjevxogt1@gmail.com>
2024-12-17 13:52:59 +0300

    Был добавлен класс нелинейных уравнений

---
+++ Models/NonLinearEquation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNU.Models
+{
+    public class NonLinearEquation
+    {
+        public string Body { get; set; }
+    }
+}
